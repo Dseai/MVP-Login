f5e0d3fa56c1591830c7a63f8011ac0b72395582
Dseai <1761239046@qq.com>
2016-06-07 17:40:08 +0800

    bb

---
+++ app/src/main/java/com/example/mvp/Login/LoginInteractor.java
@@ -0,0 +1,33 @@
+/*
+ *
+ *  * Copyright (C) 2014 Antonio Leiva Gordillo.
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *      http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+
+package com.example.mvp.Login;
+//模拟登陆的操作的接口，实现类为LoginInteractorImpl.相当于MVP模式中的Model层
+public interface LoginInteractor {
+//登陆事件的监听
+    interface OnLoginFinishedListener {
+        void onUsernameError();
+
+        void onPasswordError();
+
+        void onSuccess();
+    }
+
+    void login(String username, String password, OnLoginFinishedListener listener);
+
+}
---
+++ app/src/main/java/com/example/mvp/Login/LoginPresenterImpl.java
@@ -0,0 +1,52 @@
+
+
+package com.example.mvp.Login;
+/**
+ * 1 完成presenter的实现。这里面主要是Model层和View层的交互和操作。
+ * 2  presenter里面还有个OnLoginFinishedListener，
+ * 其在Presenter层实现，给Model层回调，更改View层的状态，
+ * 确保 Model层不直接操作View层。如果没有这一接口在LoginPresenterImpl实现的话，
+ * LoginPresenterImpl只 有View和Model的引用那么Model怎么把结果告诉View呢？
+ */
+public class LoginPresenterImpl implements LoginPresenter, LoginInteractor.OnLoginFinishedListener {
+
+    private LoginView loginView;
+    private LoginInteractor loginInteractor;
+
+    public LoginPresenterImpl(LoginView loginView) {
+        this.loginView = loginView;
+        this.loginInteractor = new LoginInteractorImpl();
+    }
+
+    @Override public void validateCredentials(String username, String password) {
+        if (loginView != null) {
+            loginView.showProgress();
+        }
+
+        loginInteractor.login(username, password, this);
+    }
+
+    @Override public void onDestroy() {
+        loginView = null;
+    }
+
+    @Override public void onUsernameError() {
+        if (loginView != null) {
+            loginView.setUsernameError();
+            loginView.hideProgress();
+        }
+    }
+
+    @Override public void onPasswordError() {
+        if (loginView != null) {
+            loginView.setPasswordError();
+            loginView.hideProgress();
+        }
+    }
+
+    @Override public void onSuccess() {
+        if (loginView != null) {
+            loginView.navigateToHome();
+        }
+    }
+}
